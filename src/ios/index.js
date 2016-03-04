52ae0bf7738b6ab52a708c252f0192ba3144e5bc
Manoj Nama <manoj.nama@outlook.com>
2016-03-04 20:54:32 +0530

    Adding some scaffolding to separate paltform specific code

---
+++ src/ios/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+import React, {
+   Component,
+   View,
+   Text,
+   StyleSheet
+}
+from 'react-native';
+
+export default class HerokuApp extends Component {
+   render() {
+      return (
+         <View>
+            <Text>
+               Hello Mates!
+            </Text>
+         </View >
+      );
+   }
+};
+
+const styles = StyleSheet.create({
+
+});
