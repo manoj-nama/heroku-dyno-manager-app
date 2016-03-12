d86faffd9f7c40436a268dd6a743ece4f17a47dd
Manoj Nama <manoj.nama@outlook.com>
2016-03-12 16:21:50 +0530

    Implemented the gravatar image url generator

--- src/common/image.manager.js
+++ src/common/image.manager.js
@@ -6,8 +6,41 @@ function ImageManager() {
 
 }
 
-ImageManager.prototype.getUrl = function (options) {
-	
+ImageManager.prototype.get = function (options) {
+	var urlBase = enums.IMAGE.BASE,
+		size = 50,
+		defaultImage = "mm",
+		rating = "pg",
+		url;
+
+	if(!options.emailHash) {
+		console.log("Error: Cannot fetch image, no email hash provided");
+		return "";
+	}
+
+	if(options.secure) {
+		urlBase = enums.IMAGE.SECURE_BASE;
+	}
+
+	if(options.default) {
+		defaultImage = options.default;
+	}
+
+	if(options.size) {
+		size = +options.size || size;
+	}
+
+	if(options.rating) {
+		rating = options.rating;
+	}
+
+	url = urlBase;
+	url += ("/" + options.emailHash);
+	url += ("?s=" + size);
+	url += ("&d=" + defaultImage);
+	url += ("&r=" + rating);
+
+	return url;
 };
 
 var manager = new ImageManager();
