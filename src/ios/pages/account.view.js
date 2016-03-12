c10fa3758ceb17d44f9931f0e8c2fc6b45b7d37b
Manoj Nama <manoj.nama@outlook.com>
2016-03-12 16:26:22 +0530

    Updated accoutnview to include API and ImageManagers, setting up default state vars

--- src/ios/pages/account.view.js
+++ src/ios/pages/account.view.js
@@ -5,10 +5,25 @@ import React, {
 	View,
 	StyleSheet,
 	Text,
+	ListView,
 	Navigator
 } from 'react-native';
 
+var enums = require("../../common/enums"),
+	ImageManager = require("../../common/image.manager"),
+	API = require("../../common/api.manager");
+
 export default class AccountPage extends Component {
+
+	constructor(props) {
+		super(props);
+		var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+		this.state = {
+			loading: true,
+			dataSource: ds.cloneWithRows([])
+		};
+	}
+
 	render() {
 		return (
 			<View style={styles.centering}>
