c35ee3a7b35531c5c346c2f367cb7336e3d2ffaa
Manoj Nama <manoj.nama@outlook.com>
2016-03-12 17:17:58 +0530

    Finished the design of accounts list view

--- src/ios/pages/account.view.js
+++ src/ios/pages/account.view.js
@@ -5,6 +5,10 @@ import React, {
 	View,
 	StyleSheet,
 	Text,
+	Image,
+	TouchableOpacity,
+	AsyncStorage,
+	ActivityIndicatorIOS,
 	ListView,
 	Navigator
 } from 'react-native';
@@ -24,10 +28,70 @@ export default class AccountPage extends Component {
 		};
 	}
 
+	componentWillMount() {
+      var _data, 
+      	_accounts = [],
+      	self = this;
+
+      AsyncStorage.getItem(enums.STORAGE.ACCOUNTS, function (err, data) {
+         if(data) {
+         	_data = JSON.parse(data);       
+         	for(let i in _data) {
+         		if(_data.hasOwnProperty(i)) {
+         			_accounts.push(_data[i]);
+         		}
+         	}
+         	self.setState({
+         		loading: false,
+         		dataSource: self.state.dataSource.cloneWithRows(_accounts)
+         	});
+         } else {
+         	console.log("No data to show, that's strange..", err);
+         }
+      });
+   }
+
+   _renderRow(rowData) {
+   	var imageUrl = ImageManager.get({
+   		emailHash: rowData.user.emailHash,
+   		size: 100,
+   	});
+		return (
+			<TouchableOpacity style={styles.listItemWrap} 
+				key={rowData.email}>
+				<View style={styles.listItem}>
+					<View style={styles.picCol}>
+						<Image 
+							style={styles.pic}
+							resizeMode={'contain'}
+							source={{uri: imageUrl}} 
+						/>
+					</View>
+					<View style={styles.info}>
+						<Text style={styles.name}>{rowData.user.name}</Text>
+						<Text style={styles.email}>{rowData.email}</Text>
+					</View>
+				</View>
+			</TouchableOpacity>
+		);
+	}
+
 	render() {
 		return (
-			<View style={styles.centering}>
-				<Text>Accounts view</Text>
+			<View style={styles.nav} automaticallyAdjustsScrollViewInsets={true}>
+				<ListView style={styles.list}
+					contentContainerStyle={styles.container}
+					pageSize={5}
+					automaticallyAdjustContentInsets={true}
+			    	dataSource={this.state.dataSource}
+			    	renderRow={this._renderRow.bind(this)} />
+
+			   {
+			   	this.state.loading ?
+			   	<View style={styles.centering}>
+						<ActivityIndicatorIOS color={'#3A1051'} size={'large'} />
+					</View> : null
+		    	}
 			</View>
 		);
 	}
@@ -38,5 +102,53 @@ var styles = StyleSheet.create({
 		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'center'
-	}
+	},
+	nav: {
+		flex: 1,
+		backgroundColor: "#f5f5f5",
+	},
+	list: {
+		paddingTop: 70,
+	},
+	container: {
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	listItemWrap: {
+		alignSelf: "stretch",
+	},
+	listItem: {
+		flex: 1,
+		alignItems: "center",
+		flexDirection: "row",
+		borderRadius: 4,
+		backgroundColor: "#fff",
+		padding: 10,
+		marginHorizontal: 10,
+		marginVertical: 5,
+	},
+	picCol: {
+		marginRight: 10,
+		width: 50,
+	},
+	pic: {
+		width: 50,
+		borderRadius: 25,
+		height: 50,
+	},
+	info: {
+		flex: 1,
+	},
+	name: {
+		fontWeight: "200",
+		flex: 1,
+		fontSize: 18,
+	},
+	email: {
+		flex: 1,
+		color: "#555",
+		paddingVertical: 3,
+		fontWeight: "200",
+		fontSize: 13,
+	},
 });
