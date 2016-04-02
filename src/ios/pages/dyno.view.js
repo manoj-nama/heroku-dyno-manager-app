b55644f7059bc54114edaf3d7ebbe9fb2bfcd5b4
Manoj Nama <manoj.nama@outlook.com>
2016-04-02 20:46:19 +0530

    Added default dynos view

---
+++ src/ios/pages/dyno.view.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import React, {
+	Component,
+	View,
+	Text,
+	StyleSheet
+} from "react-native";
+
+export default class DynoPage extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<View>
+				<Text>This is dynos view</Text>
+			</View>	
+		);
+	}
+};
+
+const styles = StyleSheet.create({
+
+});
