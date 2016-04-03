0732abc20a27384d556c42764d65a50815219e4b
Manoj Nama <manoj.nama@outlook.com>
2016-04-03 20:01:21 +0530

    INtegrated the dyno API data to listview

--- src/ios/pages/dyno.view.js
+++ src/ios/pages/dyno.view.js
@@ -6,12 +6,13 @@ import React, {
 	Text,
 	ListView,
 	InteractionManager,
+	TouchableOpacity,
 	ActivityIndicatorIOS,
 	StyleSheet
 } from "react-native";
 
 var enums = require("../../common/enums"),
-	Icon = require('react-native-vector-icons/Ionicons'),
+	Icon = require('react-native-vector-icons/MaterialIcons'),
 	API = require("../../common/api.manager");
 
 export default class DynoPage extends Component {
@@ -47,15 +48,47 @@ export default class DynoPage extends Component {
 		});
 	}
 
+	restartDyno(data) {
+
+	}
+
+	_renderRow(rowData) {
+		return (
+			<View style={styles.listItemWrap}>
+				<View style={styles.listItem}>
+					<View style={styles.picCol}>
+						<Icon name="memory" size={32} color="#444" />
+					</View>
+					<View style={styles.info}>
+						<View>
+							<Text style={styles.name}>{rowData.name}</Text>
+							<Text style={styles.email}>{rowData.size}</Text>
+						</View>						
+					</View>
+					<TouchableOpacity onPress={()=> this.restartDyno(rowData)}>
+							<View style={styles.navIcon}>
+								<Icon name="cached" size={30} color="#333" />
+							</View>
+						</TouchableOpacity>
+				</View>
+			</View>	
+		);
+	}
+
 	render() {
 		return (
-			<View style={[styles.nav, styles.centering]}>
+			<View style={styles.nav}>
 			{
 				(!this.state.isReady || this.state.loading) ? 
 				<View style={styles.centering}>
 					<ActivityIndicatorIOS color={'#444'} size={'large'} />
 				</View> :
-				<Text>This is dynos view</Text>				
+				<ListView style={styles.list}
+					contentContainerStyle={styles.container}
+					pageSize={5}
+					automaticallyAdjustContentInsets={true}
+			    	dataSource={this.state.dataSource}
+			    	renderRow={this._renderRow.bind(this)} />			
 			}
 			</View>
 		);
@@ -71,5 +104,55 @@ const styles = StyleSheet.create({
 	nav: {
 		flex: 1,
 		backgroundColor: "#eee",
+	},
+	list: {
+		paddingTop: 70,
+	},
+	container: {
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	listItemWrap: {
+		alignSelf: "stretch",
+	},
+	listItem: {
+		flex: 1,
+		alignItems: "center",
+		flexDirection: "row",
+		borderRadius: 4,
+		backgroundColor: "#fff",
+		padding: 10,
+		marginHorizontal: 10,
+		marginVertical: 5,
+	},
+	picCol: {
+		marginRight: 10,
+	},
+	pic: {
+		backgroundColor: "#ccc",
+		borderRadius: 25,
+	},
+	info: {
+		flex: 1,
+	},
+	navIcon: {
+		alignItems: "center",
+		justifyContent: "center",
+		width: 50,
+		height: 50,
+		borderRadius: 25,
+		backgroundColor: "#eee",
+	},
+	name: {
+		fontWeight: "200",
+		flex: 1,
+		fontSize: 18,
+	},
+	email: {
+		flex: 1,
+		color: "#555",
+		paddingVertical: 3,
+		fontWeight: "200",
+		fontSize: 13,
 	},
 });
