f74853cffa340cb5c4138c7953dc21ef62a1b8f7
Manoj Nama <manoj.nama@outlook.com>
2016-05-08 11:24:22 +0530

    Adding the account add page

---
+++ src/android/pages/account.add.view.js
@@ -0,0 +1,163 @@
+'use strict';
+
+import React, {
+	Component,
+	View,
+	TextInput,
+	Text,
+	AsyncStorage,
+	ProgressBarAndroid,
+	TouchableOpacity,
+	StyleSheet,
+} from "react-native";
+
+var enums = require("../../common/enums"),
+	API = require("../../common/api.manager");
+
+export default class AddAccountPage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isBusy: false,
+			authError: false,
+			accounts: {},
+			email: "",
+			password: ""
+		};
+	}
+
+	componentWillMount() {
+		let self = this;
+		AsyncStorage.getItem(enums.STORAGE.ACCOUNTS, function (err, data) {
+			let _data = {};
+         if(data) {
+         	try {
+					_data = JSON.parse(data);
+	         	self.setState({
+	         		accounts: _data
+	         	});
+         	} catch(e) {
+         		console.log(e);
+         	}
+         } else {
+         	console.log("No data to show, that's strange..", err);
+         }
+      });
+	}
+
+	addAccount() {
+		if(!this.state.isBusy && this.state.email) {
+			var response = API.login({
+				email: this.state.email,
+				password: this.state.password,
+			});
+			response.then((data) => {
+				if(data.token) {
+					var accounts = this.state.accounts,
+						self = this;
+					accounts[data.email] = data;
+					AsyncStorage.setItem(enums.STORAGE.ACCOUNTS, JSON.stringify(accounts), ()=> {
+						self.props.navigator.pop();
+					});
+				} else {
+					//auth error
+					this.setState({
+						isBusy: false,
+						authError: true,
+					});
+				}
+			}).done();
+		}
+		this.setState({isBusy: true});
+	}
+
+	render() {
+		return (
+			<View style={styles.nav}>
+				{
+					this.state.authError ?
+					<View style={styles.errWrap}>
+						<Text style={styles.error}>Authentication failed!</Text>
+					</View> : null
+				}
+				<View style={styles.form}>
+					<TextInput
+						keyboardType="email-address"
+						autoCorrect={false}
+						style={styles.txt}
+						onChangeText={(text) => this.setState({email: text})}
+						autoCapitalize="none"
+						placeholder="Email" />
+
+					<TextInput
+						style={styles.txt}
+						secureTextEntry={true}
+						autoCapitalize="none"
+						onChangeText={(text) => this.setState({password: text})}
+						placeholder="Password" />
+
+					<TouchableOpacity style={styles.loginBtn}
+						onPress={this.addAccount.bind(this)}
+						activeOpacity={0.3}>
+						<View>
+							{
+								this.state.isBusy ?
+								<ProgressBarAndroid color={'#BBA1C9'} style={[styles.centering, {height: 20}]} /> :
+								<Text style={styles.btnTxt}>Add</Text>
+							}
+						</View>
+					</TouchableOpacity>
+				</View>
+			</View>
+		);
+	}
+};
+
+const styles = StyleSheet.create({
+	nav: {
+		flex: 1,
+		backgroundColor: "#eee",
+		paddingTop: 64,
+	},
+	form: {
+		paddingTop: 20,
+	},
+	errWrap: {
+		paddingVertical: 15,
+		alignItems: "center",
+		backgroundColor: "rgba(220, 40, 40, 0.7)",
+	},
+	error: {
+		color: "#fff",
+	},
+	txt: {
+		height: 40,
+		borderBottomWidth: 1,
+		fontSize: 16,
+		padding: 10,
+		borderRadius: 5,
+		margin: 10,
+		backgroundColor: "#fff",
+		borderWidth: 1,
+    color: "#333",
+		borderColor: "#bbb",
+	},
+	loginBtn: {
+		paddingVertical: 10,
+		marginHorizontal: 70,
+		marginVertical: 20,
+		borderRadius: 5,
+		backgroundColor: "#666",
+		borderWidth: 1,
+		borderColor: "#555",
+		alignItems: 'center',
+	},
+	btnTxt: {
+		fontSize: 16,
+      color: "#fff",
+	},
+	centering: {
+		alignItems: "center",
+		justifyContent: "center",
+	}
+});
